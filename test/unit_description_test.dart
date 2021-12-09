4076c41d96fe4b89c236d8fcc80b65c6f1595bab
LambsBread <45696300+LambsBread@users.noreply.github.com>
2021-12-09 15:41:41 +0100

    test fix

--- test/unit_description_test.dart
+++ test/unit_description_test.dart
@@ -2,7 +2,9 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:persona_application/codex/survey_kit_factory.dart';
 import 'package:persona_application/codex/testCard/test_description.dart';
+import 'package:survey_kit/survey_kit.dart';
 
 
 void main(){
@@ -11,7 +13,13 @@ void main(){
     TestDescription description = new TestDescription(
         title: "hello world", image: DecorationImage(
         image: AssetImage(
-            'assets/images/bigFive.png')));
+            'assets/images/bigFive.png')), surveyKit: SurveyKit(task: new OrderedTask(id: TaskIdentifier(id: "234"), steps: [
+      InstructionStep(
+      title: 'This Test is still under development!',
+      text: 'Please try again another time :)',
+      buttonText: "I'll be back!",
+    )
+        ]), onResult: (SurveyResult ) {  },),);
     String ans = description.resolveDescription("type");
     expect(ans, "Default Text");
   });
